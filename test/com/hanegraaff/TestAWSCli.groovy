0c665e2c8791fe6172e177c3ab94f323dca124d3
Mark Hanegraaff <hanegraaff@gmail.com>
2019-12-27 09:56:10 -0500

    wip

--- test/com/hanegraaff/TestAWSCli.groovy
+++ test/com/hanegraaff/TestAWSCli.groovy
@@ -1,33 +1,29 @@
 package com.hanegraaff
 
-
-import com.hanegraaff.AWSCli
-import com.hanegraaff.IStepExecutor;
-import org.junit.Before;
-import org.junit.Test;
-
-import static org.mockito.Mockito.*;
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito
 
 /**
  * Example test class
  */
-public class TestAWSCli {
+class TestAWSCli {
     private IStepExecutor _steps
 
     @Before
-    void setup()
-        _steps = org.mockito.Mockito.mock(com.hanegraaff.IStepExecutor.class)
+    void setup(){
+        _steps = Mockito.mock(IStepExecutor.class)
     }
 
     @Test
     void test_invoke() {
-        AWSCli cli = new AWSCli(this._steps);
+        AWSCli cli = new AWSCli(this._steps)
 
-        org.mockito.Mockito.when(_steps.sh()).thenReturn(0)
+        Mockito.when(_steps.sh()).thenReturn(0)
 
         int result = cli.invoke("aws s3 ls")
 
-        assert result == 1
+        assert result == 0
 
     }
 
