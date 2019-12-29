e6f94aa1989e7e6790cba9b1f986d4e6e9b59e27
Mark Hanegraaff <hanegraaff@gmail.com>
2019-12-28 21:56:31 -0500

    wip

--- vars/failoverAuroraPGRDS.groovy
+++ vars/failoverAuroraPGRDS.groovy
@@ -11,6 +11,7 @@ import com.hanegraaff.resiliency.rds.AmazonRDS
  * @param sysid The sysid of the database in question.
  * @return
  */
+@NonCPS
 def call(String sysid) {
     LogManager.setPipelineSteps(this)
     echo "About to failover database for $sysid"
